f04fdbbf94a6aee0c79abe407dfb59954f3d5c9a
ElisabetLargo <elilargo1994@hotmsil.com>
2017-10-08 00:34:22 +0200

    Menus, different scenes and fade in-out transitions

---
+++ Assets/Scripts/FadeInScenes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInScenes : MonoBehaviour {
+	public Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtons : MonoBehaviour {
+
+	public Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void Exit(){
+		
+		Application.Quit();
+	}
+
+
+	public void Tutorial(){
+		fadeOut ();
+		StartCoroutine(changeScene(1));
+	}
+
+
+	public void StartGame(){
+		fadeOut ();
+		StartCoroutine(changeScene(2));
+
+	}
+	public void Survival(){
+		fadeOut ();
+		StartCoroutine(changeScene(3));
+
+	}
+
+	void fadeOut(){
+		//fadeOutAnim.SetBool ("Fade", true);
+		anim.Play();
+	}
+
+
+	IEnumerator changeScene(int scene){
+		yield return new WaitForSeconds (1.1f);
+
+		SceneManager.LoadScene (scene);
+	}
+}
